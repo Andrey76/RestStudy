8029bba6e9fdeb0edc16eeaa3d38aedfb1834508
Andrey76 <andr2126228215@gmail.com>
2018-01-14 14:14:52 +0200

    CRUD complete

---
+++ src/main/resources/import.sql
@@ -0,0 +1,13 @@
+INSERT INTO group_to_do (group_id, name_of_to_do) VALUES (1, 'Eating per day');
+
+INSERT INTO group_to_do (group_id, name_of_to_do) VALUES (2, 'Sport per day');
+
+INSERT INTO todo (id, business, group_todo_group_id) VALUES ( 1, 'breakfast', 1);
+
+INSERT INTO todo (id, business, group_todo_group_id) VALUES (2, 'dinner', 1);
+
+INSERT INTO todo (id, business, group_todo_group_id) VALUES (3, 'supper', 1);
+
+INSERT INTO todo (id, business, group_todo_group_id) VALUES (4, 'swimming', 2);
+
+INSERT INTO todo (id, business, group_todo_group_id) VALUES (5, 'running', 2);
